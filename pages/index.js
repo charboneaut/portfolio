e9c12a6b9e0ff02f1b5953b736267d997655e99b
Timothy Charboneau <69163325+charboneaut@users.noreply.github.com>
2021-08-24 16:03:14 -0400

    initialized w/ nxa & created simple card

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <Container className={styles.container}>
+      <Head>
+        <title>Timothy Charboneau</title>
+        <meta name="description" content="Timothy Charboneau's portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container className={styles.card}>
+        <Row>
+          <Col className={styles.border}>
+            <Col className={styles.name}>Timothy Charboneau</Col>
+            <Col className={styles.title}>Full Stack Software Engineer</Col>
+            <a
+              href="https://github.com/charboneaut"
+              className={styles.projects}
+            >
+              <Col className={styles.linkCont}>Visit my GitHub </Col>
+              <Col className={styles.linkCont}>
+                <Image
+                  src="/../public/arrow-left-circle.svg"
+                  width="32px"
+                  height="32px"
+                  className={styles.arrow}
+                />
+              </Col>
+            </a>
+            <a href="/projects" className={styles.projects}>
+              <Col className={styles.linkCont}>View my Projects</Col>
+              <Col className={styles.linkCont}>
+                <Image
+                  src="/../public/arrow-left-circle.svg"
+                  width="32px"
+                  height="32px"
+                  className={styles.arrow}
+                />
+              </Col>
+            </a>
+          </Col>
+          <Col>
+            <Image
+              src="/../public/me.jpg"
+              alt="me"
+              width="256px"
+              height="256px"
+              className={styles.pfp}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
