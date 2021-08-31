0391e2c52f49c5a7b3b29ae75fc27e7236f8e9b5
Timothy Charboneau <69163325+charboneaut@users.noreply.github.com>
2021-08-31 14:06:27 -0400

    added a fadeout to a projects section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [projectView, setView] = useState(false);
+  const [fadeClass, setClass] = useState(styles.card);
   return (
     <Container className={styles.container}>
       <Head>
@@ -11,49 +14,97 @@ export default function Home() {
         <meta name="description" content="Timothy Charboneau's portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container className={styles.card}>
-        <Row>
-          <Col className={styles.info}>
-            <Col className={styles.name}>Timothy Charboneau</Col>
-            <Col className={styles.title}>Full Stack Software Engineer</Col>
-            <a
-              href="https://github.com/charboneaut"
-              className={styles.projects}
-            >
-              <Col className={styles.linkCont}>Visit my GitHub </Col>
-              <Col className={styles.linkCont}>
-                <Image
-                  src="/../public/arrow-left-circle.svg"
-                  width="32px"
-                  height="32px"
-                  className={styles.arrow}
-                />
+      {!projectView ? (
+        <Container className={fadeClass}>
+          <Row>
+            <Col className={styles.info}>
+              <Col className={styles.name}>Timothy Charboneau</Col>
+              <Col className={styles.title}>Full Stack Software Engineer</Col>
+              <a
+                href="https://github.com/charboneaut"
+                className={styles.projects}
+              >
+                <Col className={styles.linkCont}>Visit my GitHub </Col>
+                <Col className={styles.linkCont}>
+                  <Image
+                    src="/../public/arrow-left-circle.svg"
+                    width="32px"
+                    height="32px"
+                    className={styles.arrow}
+                  />
+                </Col>
+              </a>
+              <a
+                className={styles.projects}
+                onClick={() => {
+                  setClass(styles.fade);
+                  setTimeout(() => {
+                    setView(true);
+                  }, 500);
+                  setTimeout(() => {
+                    setClass(styles.appear);
+                  }, 450);
+                }}
+              >
+                <Col className={styles.linkCont}>View my Projects</Col>
+                <Col className={styles.linkCont}>
+                  <Image
+                    src="/../public/arrow-left-circle.svg"
+                    width="32px"
+                    height="32px"
+                    className={styles.arrow}
+                  />
+                </Col>
+              </a>
+            </Col>
+            <Col className={styles.border}></Col>
+            <Col className={styles.info}>
+              <Image
+                src="/../public/me.jpg"
+                alt="me"
+                width="256px"
+                height="256px"
+                className={styles.pfp}
+              />
+            </Col>
+          </Row>
+        </Container>
+      ) : (
+        <Container className={fadeClass}>
+          <Row>
+            <Col className={styles.projects}>
+              <Col className={styles.preview}>
+                <Col className={styles.proTitle}>This the first project</Col>
+                <Col>This where a picture would be</Col>
               </Col>
-            </a>
-            <a href="/projects" className={styles.projects}>
-              <Col className={styles.linkCont}>View my Projects</Col>
-              <Col className={styles.linkCont}>
-                <Image
-                  src="/../public/arrow-left-circle.svg"
-                  width="32px"
-                  height="32px"
-                  className={styles.arrow}
-                />
+              <Col>And then a blurb about it</Col>
+            </Col>
+            <Col className={styles.projects}>
+              <Col className={styles.preview}>
+                <Col className={styles.proTitle}>This the second project</Col>
+                <Col>This where a picture would be</Col>
               </Col>
-            </a>
-          </Col>
-          <Col className={styles.border}></Col>
-          <Col className={styles.info}>
-            <Image
-              src="/../public/me.jpg"
-              alt="me"
-              width="256px"
-              height="256px"
-              className={styles.pfp}
-            />
-          </Col>
-        </Row>
-      </Container>
+              <Col>And then a blurb about it</Col>
+            </Col>
+          </Row>
+          <Row>
+            <Col className={styles.projects}>
+              <Col className={styles.preview}>
+                <Col className={styles.proTitle}>This the third project</Col>
+                <Col>This where a picture would be</Col>
+              </Col>
+              <Col>And then a blurb about it</Col>
+            </Col>
+            <Col className={styles.projects}>
+              <Col className={styles.preview}>
+                <Col className={styles.proTitle}>This the fourth project</Col>
+                <Col>This where a picture would be</Col>
+              </Col>
+              <Col>And then a blurb about it</Col>
+            </Col>
+          </Row>
+        </Container>
+      )}
     </Container>
   );
 }
